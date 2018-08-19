d3ffb87a44e1add2fbde663b585385e8fec3786e
chasebish <bishop.c@husky.neu.edu>
2018-08-19 19:29:26 -0400

    route to tracks from home page

--- src/components/TrackListItem.jsx
+++ src/components/TrackListItem.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
 
 import './component.css'
 
-const TrackListItem = ({ track, rank }) => (
-    <li onClick={() => routeSong(track)} className='list-group-item list-group-item-primary cursor-pointer'>
+const TrackListItem = ({ track, rank, history }) => (
+    <li onClick={() => routeSong(track, history)} className='list-group-item list-group-item-primary cursor-pointer'>
         <h5 className='font-weight-light'>#{rank} <u>{track.name}</u></h5>
+        {console.log(history)}
     </li>
 )
 
-const routeSong = track => {
-    console.log(track)
+const routeSong = (track, history) => {
+    history.push(`/song/${track.artist.name}/${track.name}`)
 }
 
 TrackListItem.propTypes = {
     track: PropTypes.object,
-    rank: PropTypes.number
+    rank: PropTypes.number,
+    history: PropTypes.object
 }
 
-export default TrackListItem
+export default withRouter(TrackListItem)
