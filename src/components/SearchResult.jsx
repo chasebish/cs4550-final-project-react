70eddfdc9978bff6b6e25b023b8240d277ccefe1
dstein <stein.davi@husky.neu.edu>
2018-08-19 17:16:48 -0400

    search is good

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -4,40 +4,38 @@ import PropTypes from 'prop-types'
 
 export default class SearchResult extends React.Component {
 
-    song = {};
-    image = '';
-    artist = '';
-    title = '';
-    wiki = '';
+    state = {
+        song : {},
+        image : '',
+        artist : '',
+        title : '',
+        wiki : '',
+    }
 
     componentDidMount() {
-        this.setSong(this.props.song)
-    }
-
-    componentWillReceiveProps(newProps) {
-        this.setSong(newProps.song)
-        if (typeof this.song.name !== 'undefined') {
-            if (this.song.image) {
-                this.setImage(this.song.image[2]['#text'])
+        this.setState({song: this.props.song}, () => {
+            if (typeof this.state.song.name !== 'undefined') {
+                if (this.state.song.image) {
+                    this.setImage(this.state.song.image[2]['#text'])
+                }
+                this.setTitle(this.state.song.name)
+                this.setArtist(this.state.song.artist)
             }
-            this.setTitle(this.song.name)
-            this.setArtist(this.song.artist)
-        }
+        })
     }
 
-    setSong = (song) => this.song = song;
-    setImage = (url) => this.image = url;
-    setTitle = (title) => this.title = title;
-    setArtist = (artist) => this.artist = artist;
+    setImage = (url) => this.setState({ image: url })
+    setTitle = (title) => this.setState({title: title})
+    setArtist = (artist) => this.setState({artist: artist})
 
     render() {
         return (
             <div>
                 <div className="float-left mr-3">
-                    <img className="mr-3 float-left" src={this.image}></img>
+                    <img className="mr-3 float-left" src={this.state.image}></img>
                 </div>
                 <div>
-                    <Link to={`/song/${this.artist}/${this.title}`}><h3>{this.artist} - {this.title}</h3></Link>
+                    <Link to={`/song/${this.props.song.artist}/${this.props.song.name}`}><h3>{this.props.song.artist} - {this.props.song.name}</h3></Link>
                 </div>
             </div>
         )
