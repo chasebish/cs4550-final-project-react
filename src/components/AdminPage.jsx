08b31aba6b46b2dccb24533e963e8fb1fc3e7d87
Annie <annienero93@gmail.com>
2018-08-19 16:29:56 -0400

    add, edit, delete users

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -7,9 +7,10 @@ export default class AdminPage extends React.Component {
         super(props)
         this.userService = UserService.instance
         this.state = {
-            users: []
+            users: [],
+            editing: false,
+            role: "REVIEWER"
         }
-        this.renderUsers = this.renderUsers.bind(this);
     }
 
     componentDidMount() {
@@ -21,29 +22,138 @@ export default class AdminPage extends React.Component {
             })
     }
 
-    renderUsers() {
+    renderUsers = () => {
         var rows = this.state.users.map(user =>
-                <tr>
-                    <td>{user.username}</td>
-                    <td>{user.password}</td>
-                    <td>{user.firstName}</td>
-                    <td>{user.lastName}</td>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                    <td>
-                        <span class="btn btn-primary">Delete</span>
-                    </td>
-                    <td>
-                        <span class="btn btn-primary">Edit</span>
-                    </td>
-                </tr>
+            <tr>
+                <td>{user.username}</td>
+                <td>{user.password}</td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>
+                    <span class="btn btn-outline-danger" onClick={() => this.onDeleteClicked(user.id)}>Delete</span>
+                </td>
+                <td>
+                    <span class="btn btn-outline-info" onClick={() => this.onEditClicked(user)}>Edit</span>
+                </td>
+            </tr>
         )
         return rows
     }
 
+    onAddClicked = () => {
+        this.userService.addUser({
+            username: this.state.username,
+            password: this.state.password,
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            role: this.state.role
+        })
+            .then(() => {
+                this.userService.findAllUsers()
+                    .then(users => {
+                        this.setState({
+                            users: users,
+                            username: "",
+                            password: "",
+                            firstName: "",
+                            lastName: "",
+                            email: ""
+                        })
+                    })
+            })
+    }
+
+    onDeleteClicked = (id) => {
+        this.userService.deleteUser(id)
+            .then(() => {
+                this.userService.findAllUsers()
+                    .then(users => {
+                        this.setState({
+                            users: users
+                        })
+                    })
+            })
+    }
+
+    onEditClicked = (user) => {
+        this.setState({
+            userId: user.id,
+            editing: true,
+            username: user.username,
+            password: user.password,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            role: user.role
+        })
+    }
+
+    onSaveClicked = () => {
+        this.userService.updateUser(this.state.userId, {
+            username: this.state.username,
+            password: this.state.password,
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            email: this.state.email,
+            role: this.state.role
+        }).then(() => {
+            this.userService.findAllUsers()
+                .then(users => {
+                    this.setState({
+                        users: users,
+                        editing: false,
+                        username: "",
+                        password: "",
+                        firstName: "",
+                        lastName: "",
+                        email: ""
+                    })
+                })
+        })
+    }
+
+    onUsernameUpdated = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    onPasswordUpdated = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    onFirstNameUpdated = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    onLastNameUpdated = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    onEmailUpdated = (event) => {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    onRoleUpdated = (event) => {
+        this.setState({
+            role: event.target.value
+        })
+    }
+
     render() {
         return (
-            <div>
+            <div class="table-responsive">
                 <table class="table">
                     <thead>
                         <tr>
@@ -58,21 +168,23 @@ export default class AdminPage extends React.Component {
                         </tr>
                     </thead>
                     <tr>
-                        <td><input id="usernameFld" class="form-control"
-                            placeholder="Username" /></td>
+                        <td><input id="usernameFld" class="form-control" type="text"
+                            placeholder="Username" onChange={this.onUsernameUpdated} value={this.state.username} /></td>
                         <td><input id="passwordFld" class="form-control"
-                            placeholder="Password" /></td>
+                            placeholder="Password" onChange={this.onPasswordUpdated} value={this.state.password} /></td>
                         <td><input id="firstNameFld" class="form-control"
-                            placeholder="First Name" /></td>
+                            placeholder="First Name" onChange={this.onFirstNameUpdated} value={this.state.firstName} /></td>
                         <td><input id="lastNameFld" class="form-control"
-                            placeholder="Last Name" /></td>
+                            placeholder="Last Name" onChange={this.onLastNameUpdated} value={this.state.lastName} /></td>
                         <td><input class="form-control"
-                            placeholder="Email" /></td>
-                        <td><input class="form-control"
-                            placeholder="Role" /></td>
-                        <td>
-                            <span class="btn btn-primary">Add</span>
-                            <span class="btn btn-primary">Save</span>
+                            placeholder="Email" onChange={this.onEmailUpdated} value={this.state.email} /></td>
+                        <td><select class="form-control" onChange={this.onRoleUpdated} value={this.state.role}>
+                            <option value="REVIEWER">Reviewer</option>
+                            <option value="ARTIST">Artist</option>
+                            <option value="ADMIN">Admin</option>
+                        </select></td>
+                        <td><span class="btn btn-outline-info" onClick={() => this.onAddClicked()} hidden={this.state.editing}>Add</span>
+                            <span class="btn btn-outline-info" onClick={() => this.onSaveClicked()} hidden={!this.state.editing}>Save</span>
                         </td>
                     </tr>
                     <tbody>
@@ -80,7 +192,7 @@ export default class AdminPage extends React.Component {
                     </tbody>
                 </table>
 
-            </div>
+            </div >
         )
     }
 }
