31ed8be5d6e15a04b24722f28b80d091ab968c0a
chasebish <bishop.c@husky.neu.edu>
2018-08-19 22:04:05 -0400

    route to public profile

--- src/components/ArtistListItem.jsx
+++ src/components/ArtistListItem.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
 
 import './component.css'
 
-const ArtistListItem = ({ artist, rank }) => (
-    <li onClick={() => console.log(artist)} className='list-group-item list-group-item-info cursor-pointer'>
+const ArtistListItem = ({ artist, rank, history }) => (
+    <li onClick={() => routeArtist(artist, history)} className='list-group-item list-group-item-info cursor-pointer'>
         <h5 className='font-weight-light'>#{rank} <u>{artist.name}</u></h5>
     </li>
 )
 
-ArtistListItem.propTypes = {
-    artist: PropTypes.object,
-    rank: PropTypes.number
+const routeArtist = (artist, history) => {
+    history.push(`/public-profile/${artist.name}`)
 }
 
-export default ArtistListItem
+ArtistListItem.propTypes = {
+    artist: PropTypes.object,
+    rank: PropTypes.number,
+    history: PropTypes.object
+}
+
+export default withRouter(ArtistListItem)
